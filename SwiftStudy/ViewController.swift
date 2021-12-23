9156a8135d36d6d4dd9bafe07042fc18188b38ec
leepc777 at hotmail <leepc777@hotmail.com>
2021-12-22 18:07:25 -0800

    Initial Commit

---
+++ SwiftStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftStudy
+//
+//  Created by myHotmail on 12/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
